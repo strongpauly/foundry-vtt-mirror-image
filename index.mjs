9c815eb571a691cfaac3666c02d38b5c6f283892
Paul Potsides <paul@koble.ai>
2023-05-06 19:54:43 +0100

    Update macro properly

--- index.mjs
+++ index.mjs
@@ -15,15 +15,16 @@ Hooks.once('ready', async function(){
   window.MirrorImage = new MirrorImage();
   let macro = game.macros.getName(MACRO_NAME);
   const macroData = await getMacroData();
-  if(!macro || macro.flags?.version !== MACRO_VERSION) {
-    console.log(`Creating macro ${MACRO_NAME}`)
+  if(!macro) {
+    console.log(`Creating macro ${MACRO_NAME}`);
     macro = await Macro.create(Object.assign({
       "type": "script",
       "scope": "global",
       "permission": "NONE",
       "folder": null
     }, macroData));
-  } else {
+  } else if(macro.flags?.version !== MACRO_VERSION) {
+    console.log(`Updating macro ${MACRO_NAME} to ${MACRO_VERSION}`);
     macro = Object.assign(macro, macroData)
   }
   game.macros.set(macro.name, macro);
